9f27555041134972038e9e77cdf3a939d4dd645e
istudgamer <binayaxxxu@gmail.com>
2026-05-28 20:12:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,31 @@ import {
 
 const gamesGrid = document.getElementById("gamesGrid");
 
+const popup = document.getElementById("popup");
+
+const closeBtn = document.getElementById("closeBtn");
+
+const popupIcon = document.getElementById("popupIcon");
+
+const popupTitle = document.getElementById("popupTitle");
+
+const popupThumb = document.getElementById("popupThumb");
+
+const popupVersion = document.getElementById("popupVersion");
+
+const popupFeatures = document.getElementById("popupFeatures");
+
+const popupDownloads = document.getElementById("popupDownloads");
+
+const downloadBtn = document.getElementById("downloadBtn");
+
+const tutorialBtn = document.getElementById("tutorialBtn");
+
 async function loadGames() {
 
   gamesGrid.innerHTML = "Loading...";
 
-  const querySnapshot = await getDocs(collection(db, "Games"));
+  const querySnapshot = await getDocs(collection(db, "games"));
 
   gamesGrid.innerHTML = "";
 
@@ -32,10 +52,53 @@ async function loadGames() {
       <p>${game.version}</p>
 
       <p>${game.downloads} Downloads</p>
+
     `;
+
+    // CLICK EVENT
+    card.addEventListener("click", () => {
+
+      popup.classList.remove("hidden");
+
+      popupIcon.src = game.icon;
+
+      popupTitle.innerText = game.title;
+
+      popupThumb.src = game.thumbnail;
+
+      popupVersion.innerText = "Version: " + game.version;
+
+      popupFeatures.innerText =
+        "MOD Features: " + game.modFeatures;
+
+      popupDownloads.innerText =
+        "Downloads: " + game.downloads;
+
+      downloadBtn.href = game.downloadLink;
+
+      tutorialBtn.href = game.youtubeLink;
+    });
 
     gamesGrid.appendChild(card);
   });
 }
+
+// CLOSE POPUP
+closeBtn.addEventListener("click", () => {
+
+  popup.classList.add("hidden");
+
+});
+
+// CLOSE WHEN CLICK OUTSIDE
+popup.addEventListener("click", (e) => {
+
+  if (e.target === popup) {
+
+    popup.classList.add("hidden");
+
+  }
+
+});
 
 loadGames();
